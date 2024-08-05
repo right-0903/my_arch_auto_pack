521bc2424304505f7fd4394dc85b496cb4f3b2d0
nuvole <mitltlatltl@gmail.com>
2024-08-05 12:20:19 +0800

    utils: add a handler for invalid package names

---
+++ utils/change_invalid_name.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Specify the directory to process
+TARGET_DIR="$1"
+
+# Check if the directory parameter is provided
+if [ -z "$TARGET_DIR" ]; then
+  echo "$0: Please provide a directory path"
+  exit 1
+fi
+
+# Ensure the target directory exists
+if [ ! -d "$TARGET_DIR" ]; then
+  echo "$0: The specified directory does not exist"
+  exit 1
+fi
+
+# Define invalid characters to be removed, in case failed to upload artifacts
+# Invalid characters include:  Double quote ", Colon :, Less than <, Greater than >,
+# Vertical bar |, Asterisk *, Question mark ?, Carriage return \r, Line feed \n
+INVALID_CHARS='["<>|*?:\r\n]'
+
+# Iterate over all files in the specified directory
+for FILE in "$TARGET_DIR"/*; do
+  # Extract the filename and directory name
+  DIRNAME=$(dirname "$FILE")
+  BASENAME=$(basename "$FILE")
+
+  # Remove invalid characters
+  CLEAN_NAME=$(echo "$BASENAME" | tr -d $INVALID_CHARS)
+
+  # If the cleaned filename differs from the original, rename the file
+  if [ "$BASENAME" != "$CLEAN_NAME" ]; then
+    mv "$DIRNAME/$BASENAME" "$DIRNAME/$CLEAN_NAME"
+    echo "$0: Renamed: $BASENAME -> $CLEAN_NAME"
+  fi
+done
+
+echo "$0: All files processed"
--- utils/post_update.sh
+++ utils/post_update.sh
@@ -1,15 +1,24 @@
 #! /bin/bash
 
-echo "post_update"
-cd "$GITHUB_WORKSPACE"/repos
+git_push() {
+    echo "post_update"
+    cd "$GITHUB_WORKSPACE"/repos
 
-git config --global user.name "nuvole"
-git config --global user.email "github-actions[bot]@users.noreply.github.com"
-git add update_list*
-git add */version
+    git config --global user.name "nuvole"
+    git config --global user.email "github-actions[bot]@users.noreply.github.com"
+    git add update_list*
+    git add */version
 
-update_list=$(cat 'update_list' | awk '{print $1}')
+    update_list=$(cat 'update_list' | awk '{print $1}')
 
-cd "$GITHUB_WORKSPACE"
-localtime="$(timedatectl | grep 'Local time' | awk '{print $4"_"$5}')"
-git commit -m "$localtime: $update_list"
+    cd "$GITHUB_WORKSPACE"
+    localtime="$(timedatectl | grep 'Local time' | awk '{print $4"_"$5}')"
+    git commit -m "$localtime: $update_list"
+}
+
+rename_invalid_name() {
+    "$GITHUB_WORKSPACE/utils/change_invalid_name.sh" "$GITHUB_WORKSPACE/builddir/root.x86_64/home/nuvole/prod/"
+}
+
+git_push
+rename_invalid_name
